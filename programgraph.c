9b9032370bc5ac4b68e2e36e5bbe98e5408311cd
swityx <72469885+swityx@users.noreply.github.com>
2020-12-15 18:27:39 +0300

    Update programgraph.c

--- programgraph.c
+++ programgraph.c
@@ -1,114 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-FILE* f;
+FILE * fo;
+int main()
+{
+    printf("Enter size vertex*edge:\n");
+    int vertex, edge;
+    while (scanf("%d", &vertex) != 1 || vertex < 0)
+        scanf("%*[^\n]");
+    while (scanf("%d", &edge) != 1 || edge < 0)
+        scanf("%*[^\n]");
+    int mass[vertex][edge];
 
-int main(void)
-{   setlocale(LC_ALL, "Rus");
-    printf("введите количество ребер и вершин\n");
-    int rb, vr;
-
-    scanf("%d",&rb);
+    printf("Enter matrix:\n");
+    for (int i = 0; i < vertex; i++)
+        for (int j = 0; j < edge; j++)
+            while (scanf("%d", &(mass[i][j])) != 1 || (mass[i][j] != 0 && mass[i][j] != 1))
+                scanf("%*[^\n]");
+    int degree;
+    int md[vertex];
+    printf("Enter vertex degree:\n");
+    scanf("%d", &degree);
+        for (int i = 0; i < vertex; i++){
+            int counter = 0;
+            for (int j = 0; j < edge; j++){
+                if (mass[i][j] == 1) {
+                counter++;
+                }
+            if (degree == counter)
+                md[i] = i+1;
+             else
+                md[i] = 0;
+            }
+        }
+    printf("Vertices with %d degree:", degree);
+    for (int i = 0; i < vertex; i++){
+        if (md[i] != 0)
+            printf("%d ",md[i]);
+    }
     printf("\n");
-    scanf("%d",&vr);
-
-    int m[rb][vr];
-
-        for (int i = 0; i < rb; i++) {
-            for (int j = 0; j < vr; j++){
-                scanf("%d",&m[i][j]);
-                }
-        }
-
-        f = fopen("text.dot","w");
-        if (f == NULL)
-            return 0;
-
-        fprintf(f,"graph MyGraf {\n");
-
-        for (int i = 0; i < rb; i++)
-        {
-            int x1 = -1;
-            int x2 = -1;
-
-            for (int j = 0; j < vr; j++)
-            {
-                if (m[i][j] == 1 && x1 == -1 )
-                    x1 = j+1;
-                else if (m[i][j] == 1 && x2 == -1 )
-                    x2 = j+1;
-                 else if (m[i][j] == 2 )
-                    x2 = x1 = j+1;
+    fo = fopen("test.dot","wt");
+    fprintf(fo, "graph Graph {\n");
+    for (int j = 0; j < edge; ++j)  {
+        int x1 = -1;
+        int x2 = -1;
+        for (int i = 0; i < vertex; ++i)
+            if (mass[i][j] == 1) {
+                if  (x1 != -1)
+                    x2 = i;
+                else
+                    x1 = i;
             }
-            if (x1 == -1)
-                fprintf(f,"%d;\n",x2);
-            else if (x2 == -1)
-                fprintf(f,"%d;\n",x1);
-            else
-                fprintf(f,"%d -- %d\n",x1,x2);
+        if (x1!=-1 && x2!=-1)
+            fprintf( fo, "  %d -- %d;\n", x1+1, x2+1);
+    }
+    fprintf(fo, "}\n");
+    fclose(fo);
 
-        }
 
-        fprintf(f,"}");
-
-        int kols = rb;
-
-        // finding loops and multiple edges
-        for (int i = 0; i < rb; i++)
-        {
-            for (int j = 0; j < vr; j++)
-            {
-                if (m[i][j] == 2)
-                    kols--;
+    int conectx[vertex][vertex];
+    for (int j = 0; j < edge; ++j) {
+        int x1 = -1;
+        int x2 = -1;
+        for (int i = 0; i < vertex; ++i)
+            if (mass[i][j] == 1) {
+                if (x1 != -1)
+                    x2 = i;
+                else
+                    x1 = i;
             }
-        }
-        int sovp;
-        for(int p = 0; p < rb; p++)
-        {
-            kols++;
-            for (int i = 0; i < rb; i++)
-            {
-                sovp = 0;
-                for (int j = 0; j < vr; j++)
-                {
-                    if ( m[p][j] == m[i][j])
-                    sovp++;
-                }
-                if (sovp == rb)
-                kols--;
-            }
-        }
-        int sum1 = 0;
-        for (int i = 0; i < rb; i++)
-        {
-            sum1 = 0;
-            for (int j = 0; j < vr; j++)
-            {
-                if ( m[i][j] > 0)
-                sum1++;
-            }
-            if (sum1 == 1)
-            kols--;
-        }
-
-        if (vr == 2)
-        {
-            if (kols >= 1)
-                printf("Граф подключен");
-            else
-                printf("Граф не подключен");
-            return 0;
-        }
-            if (vr == 3)
-        {
-            if (kols >= 2)
-                printf("The graph is connected");
-            else
-                printf("The graph is not connected");
-            return 0;
-        }
+        conectx[x1][x2] = 1;
+    }
 
 
+    int count = 0;
+    for (int i = 0; i < vertex; ++i)
+        for (int j = 0; j < vertex; ++j)
+            if (conectx[i][j] == 1)
+                ++count;
 
+    //printf("%d\n", count);
+
+    if ((vertex-1)*(vertex-2)/2 < count)
+        printf("Graph is connected");
+    else
+        printf("Graph is not connected");
+
+    system("dot -Tpng test.dot -o test.png");
     return 0;
 }
